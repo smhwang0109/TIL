6f221c8a3f2da3345420de66896f876399e28e48
Soomin Hwang <smhwang0109@gmail.com>
2020-03-17 21:37:23 +0900

    Add Momentum

--- WEB/Nomadcoder/VanilaJS/Momentum/todo.js
+++ WEB/Nomadcoder/VanilaJS/Momentum/todo.js
@@ -4,15 +4,41 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos"
 
+let toDos = [];
+
+function deleteTodo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos =  toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     const li  =  document.createElement("li");
     const delBtn = document.createElement("button")
-    delBtn.innerText= "❌";
     const span = document.createElement("span");
-    span.innerText = text
+    const newId = toDos.length + 1;
+    delBtn.innerText= "❌";
+    delBtn.addEventListener("click", deleteTodo);
+    span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
     toDoList.appendChild(li);
+    li.id = newId
+    const toDoObj = {
+        text : text,
+        id : newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -22,9 +48,12 @@ function handleSubmit(event) {
     toDoInput.value = "";
 }
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
