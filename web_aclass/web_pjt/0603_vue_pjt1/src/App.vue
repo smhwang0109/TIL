1764e9080be07d9f2aa880400d1d7b6fdac4adb7
Soomin Hwang <smhwang0109@gmail.com>
2020-06-03 18:50:49 +0900

    Add 0603_vue_pjt1

---
+++ web_aclass/web_pjt/0603_vue_pjt1/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">Movie App</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link to="/">Movie</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/video">Video</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.nav-item {
+  padding: 10px;
+}
+
+.nav-item a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+.nav-item a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
