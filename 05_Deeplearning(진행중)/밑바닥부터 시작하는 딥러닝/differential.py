b55d63f0eb34d04be14ed338fa65f7b8f8854b31
smhwang0109 <smhwang0109@gmail.com>
2020-09-26 13:00:15 +0900

    Add differential file

---
+++ 05_Deeplearning(진행중)/밑바닥부터 시작하는 딥러닝/differential.py
@@ -0,0 +1,11 @@
+import numpy as np
+from modules.differential import numerical_gradient
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([3.0, 0.0])))
+
