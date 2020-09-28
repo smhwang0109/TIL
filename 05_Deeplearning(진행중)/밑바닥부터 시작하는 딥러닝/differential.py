ca4320b947ec63355c041acc2464d791c82c4a6f
smhwang0109 <smhwang0109@gmail.com>
2020-09-28 19:00:15 +0900

    Add gradient_descent test

--- 05_Deeplearning(진행중)/밑바닥부터 시작하는 딥러닝/differential.py
+++ 05_Deeplearning(진행중)/밑바닥부터 시작하는 딥러닝/differential.py
@@ -1,11 +1,20 @@
 import numpy as np
-from modules.differential import numerical_gradient
+from modules.differential_function import numerical_gradient, gradient_descent
+
 
 def function_2(x):
     return x[0]**2 + x[1]**2
-
 
 print(numerical_gradient(function_2, np.array([3.0, 4.0])))
 print(numerical_gradient(function_2, np.array([0.0, 2.0])))
 print(numerical_gradient(function_2, np.array([3.0, 0.0])))
 
+# 적절한 lr(learning rate: 학습률)
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
+# 과도한 lr
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=10.0, step_num=100))
+# 너무 적은 lr
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=1e-10, step_num=100))
