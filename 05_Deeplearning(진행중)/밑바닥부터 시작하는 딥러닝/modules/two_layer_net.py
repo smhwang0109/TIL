13ae6c71a0aa8cf8e90fd9a52df42dfa301955a6
smhwang0109 <smhwang0109@gmail.com>
2020-10-02 12:00:15 +0900

    Add train_neuralnet

--- 05_Deeplearning(진행중)/밑바닥부터 시작하는 딥러닝/modules/two_layer_net.py
+++ 05_Deeplearning(진행중)/밑바닥부터 시작하는 딥러닝/modules/two_layer_net.py
@@ -50,21 +50,21 @@ class TwoLayerNet:
         return grads
 
 
-net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
-print(net.params['W1'].shape)
-print(net.params['b1'].shape)
-print(net.params['W2'].shape)
-print(net.params['b2'].shape)
-
-x = np.random.randn(100, 784)
-y = net.predict(x)
-t = np.random.randn(100, 10)
-
-grads = net.numerical_gradient(x, t)
-
-print('---------------')
-print(grads['W1'].shape)
-print(grads['b1'].shape)
-print(grads['W2'].shape)
-print(grads['b2'].shape)
-print('---------------')
+# net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+# print(net.params['W1'].shape)
+# print(net.params['b1'].shape)
+# print(net.params['W2'].shape)
+# print(net.params['b2'].shape)
+#
+# x = np.random.randn(100, 784)
+# y = net.predict(x)
+# t = np.random.randn(100, 10)
+#
+# grads = net.numerical_gradient(x, t)
+#
+# print('---------------')
+# print(grads['W1'].shape)
+# print(grads['b1'].shape)
+# print(grads['W2'].shape)
+# print(grads['b2'].shape)
+# print('---------------')
