34ce305deb3d2df4af95c45a17e75cd0dd8e3e39
smhwang0109 <smhwang0109@gmail.com>
2020-10-03 13:00:15 +0900

    Add train_neuralnet accuracy

--- 05_Deeplearning(진행중)/밑바닥부터 시작하는 딥러닝/06_train_neuralnet.py
+++ 05_Deeplearning(진행중)/밑바닥부터 시작하는 딥러닝/06_train_neuralnet.py
@@ -2,9 +2,10 @@ import numpy as np
 from modules.mnist import load_mnist
 from modules.two_layer_net import TwoLayerNet
 
+
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-train_loss_list = []
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 # 하이퍼파라미터
 iters_num = 10000 # 반복 횟수
@@ -13,7 +14,12 @@ train_size = x_train.shape[0]
 batch_size = 100 # 미니배치 크기
 learning_rate = 0.1
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+# 1에폭당 반복 수
+iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
     # 미니배치 획득
@@ -33,4 +39,11 @@ for i in range(iters_num):
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
     print(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
 
