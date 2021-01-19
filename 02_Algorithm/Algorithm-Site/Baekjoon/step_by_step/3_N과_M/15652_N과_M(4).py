d43eff8edb570f145ccd33d965a454cb316e92a2
Soomin Hwang <smhwang0109@gmail.com>
2021-01-20 01:21:21 +0900

    Update Algo Baekjoon 15652

--- 02_Algorithm/Algorithm-Site/Baekjoon/step_by_step/3_N과_M/15652_N과_M(4).py
+++ 02_Algorithm/Algorithm-Site/Baekjoon/step_by_step/3_N과_M/15652_N과_M(4).py
@@ -2,20 +2,17 @@ import sys
 
 input = lambda : sys.stdin.readline().strip()
 
-def recur(m):
+def recur(num, m):
     global N
     if not m:
         print(' '.join(map(str, num_list)))
         return
-    for i in range(num_list[-1], N + 1):
+    for i in range(num, N + 1):
         num_list.append(i)
-        recur(m - 1)
+        recur(i, m - 1)
         num_list.pop()
 
 N, M = map(int, input().split())
 num_list = []
 
-for num in range(1, N + 1):
-    num_list.append(num)
-    recur(M - 1)
-    num_list.pop()
+recur(1, M)
