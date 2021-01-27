fdac91a0ea0b05d84e25603d5b81c4fe685e13b7
smhwang0109 <smhwang0109@gmail.com>
2021-01-27 16:49:17 +0900

    Add Algo Beakjoon BarkingDog 1406

--- 02_Algorithm/Algorithm-Site/Baekjoon/barking_dog/02강-배열과 연결 리스트/1406_에디터.py
+++ 02_Algorithm/Algorithm-Site/Baekjoon/barking_dog/02강-배열과 연결 리스트/1406_에디터.py
@@ -2,25 +2,75 @@ import sys
 
 input = lambda : sys.stdin.readline().strip()
 
+
+class Node:
+    def __init__(self, alpha):
+        self.alpha = alpha
+        self.pre = None
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node('head')
+
+    def left(self, node):
+        if node == self.head:
+            return node
+        return node.pre
+
+    def right(self, node):
+        if not node.next:
+            return node
+        return node.next
+
+    def pop(self, node):
+        if node == self.head:
+            return node
+        pre_node = node.pre
+        next_node = node.next
+        pre_node.next = next_node
+        if next_node:
+            next_node.pre = pre_node
+        return pre_node
+
+    def append(self, cur_node, node):
+        next_node = cur_node.next
+        cur_node.next = node
+        node.pre = cur_node
+        if next_node:
+            next_node.pre = node
+        node.next = next_node
+        return node
+
+    def print_all(self):
+        node = self.head.next
+        while node:
+            print(node.alpha, end='')
+            node = node.next
+        return
+
+
+
 S = list(input())
 M = int(input())
-idx = len(S) - 1
+linked_list = LinkedList()
+pre_node = linked_list.head
+
+for idx in range(len(S)):
+    node = Node(S[idx])
+    linked_list.append(pre_node, node)
+    pre_node = node
+
 for _ in range(M):
     command = input()
     if command == 'L':
-        idx -= 1
-        if idx < -1:
-            idx = -1
-        continue
+        node = linked_list.left(node)
+    elif command == 'D':
+        node = linked_list.right(node)
     elif command == 'B':
-        if idx != -1:
-            S.pop(idx)
-            idx -= 1
-        continue
-    elif len(command) == 3:
-        S = S[:idx + 1] + [command[-1]] + S[idx+1:]
-    idx += 1
-    if idx >= len(S):
-        idx = len(S) - 1
+        node = linked_list.pop(node)
+    else:
+        new_node = Node(command[-1])
+        node = linked_list.append(node, new_node)
 
-print(''.join(S))
+linked_list.print_all()
